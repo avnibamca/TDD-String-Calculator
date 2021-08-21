0619b5cb7faf240636fd617983968091a6f23a0f
Avniba Chudasma <77261426+avnibamca@users.noreply.github.com>
2021-08-21 18:01:52 +0530

    source

---
+++ source/main/NegativeNumException.java
@@ -0,0 +1,16 @@
+package main;
+
+public class NegativeNumException {
+    public static class NegativesNotAllowed extends RuntimeException {
+        public NegativesNotAllowed(String neg) {
+            super(neg);
+        }
+    }
+
+    public static void throwExceptionIfNegativeExists(String negative_num) {
+        if (negative_num_str != "") {
+            throw new NegativesNotAllowed("Negatives Not Allowed. Numbers are: " + negative_num_str);
+        }
+    }
+
+}
